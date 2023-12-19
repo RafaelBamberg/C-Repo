f74243d9c30d93af02ff1de54fc81facb16a96a4
RafaelBamberg <raafaelbamberg@gmail.com>
2023-12-19 19:52:08 -0300

    att

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
-int main()
-{
-    printf("Hello World!\n");
+
+int main() {
+    int age = 123; // Numero inteiro
+    char grade = 'A'; // Caractere, aspas simples
+    char name[] = "string"; // Array de caracteres (na teoria uma string), aspas duplas
+    float gpa = 3.5; // Numero com ponto flutuante
+    double gpa2 = 3.5; // Numero com ponto flutuante com mais precisao
+
+    // %d para inteiro, %c para caractere, %s para string, %f para float, %lf para double
+
+    printf("MOSTRANDO %s\n", name);
+    printf("MOSTRANDO %d\n", age);
+    printf("MOSTRANDO %c\n", grade);
+    printf("MOSTRANDO %f\n", gpa);
+    printf("MOSTRANDO %lf\n", gpa2);
+    return 0;
 }
